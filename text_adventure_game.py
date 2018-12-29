e62f62141a4ff5654dd9a0b5fd3e60e5a406b2e3
Andrew <andrew@Andrews-iMac.local>
2018-12-29 11:57:29 -0800

    A game to practice concepts

---
+++ text_adventure_game.py
@@ -0,0 +1,10 @@
+# game ideas...
+# cave adventure game, the point of which is to find a way out, to the light..
+
+print("""You awaken in a cave, the only light is that of the lattern nearby on 
+the cavern floor. You do not know how you got here, all you remember is falling
+asleep in your bed. after a brief look around with the lattern in your hand 
+you start to notice that the cave continues in 2 different directions.""")
+
+print("1. You choose to go down the right hand path")
+print("2. You choose to go down the left hand path")
